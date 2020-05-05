e1c13fd96bd018bd2fd27db2d14984e0df3edaa3
Ilya Scherbina <sch@myfantasy.ru>
2020-05-05 07:43:40 +0300

    add error move generator

---
+++ error.go
@@ -0,0 +1,87 @@
+package mft
+
+// Error type with internal
+type Error struct {
+	Code          int    `json:"code,omitempty"`
+	Msg           string `json:"msg,omitempty"`
+	InternalError string `json:"ie,omitempty"`
+}
+
+// ErrorCommonCode - no code error
+const ErrorCommonCode int = 50000
+
+// Error implement error interface
+func (e *Error) Error() string {
+	if e == nil {
+		return ""
+	}
+
+	if e.InternalError == "" {
+		return e.Msg
+	}
+	return e.Msg + "\n" + e.InternalError
+}
+
+// ErrorCS make Error from string
+func ErrorCS(code int, err string) *Error {
+	return &Error{
+		Msg:  err,
+		Code: code,
+	}
+}
+
+// ErrorS make Error from string
+func ErrorS(err string) *Error {
+	return &Error{
+		Msg:  err,
+		Code: ErrorCommonCode,
+	}
+}
+
+// ErrorCE make Error from any error
+func ErrorCE(code int, err error) *Error {
+	return &Error{
+		Msg:  err.Error(),
+		Code: code,
+	}
+}
+
+// AppendS append next error level saving code
+func (e *Error) AppendS(errs string) *Error {
+	return &Error{
+		Msg:           errs,
+		InternalError: e.Error(),
+		Code:          e.Code,
+	}
+}
+
+// AppendE append next error level saving code
+func (e *Error) AppendE(errs error) *Error {
+	return &Error{
+		Msg:           errs.Error(),
+		InternalError: e.Error(),
+		Code:          e.Code,
+	}
+}
+
+// ErrorE make Error from any error
+func ErrorE(err error) *Error {
+	return &Error{
+		Msg:  err.Error(),
+		Code: ErrorCommonCode,
+	}
+}
+
+// ErrorNew - Create new Error from msg and another error
+func ErrorNew(msg string, internalError error) *Error {
+	return &Error{
+		Msg:           msg,
+		InternalError: internalError.Error(),
+		Code:          ErrorCommonCode,
+	}
+}
+
+// ErrorNew2 - Create new Error
+func ErrorNew2(msg string, internalError error, internal2Error error) *Error {
+	return ErrorNew(msg, ErrorNew(internalError.Error(), internal2Error))
+}
---
+++ error_test.go
@@ -0,0 +1,31 @@
+package mft
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+)
+
+func TestErrors(t *testing.T) {
+	errV0 := errors.New("ttt")
+
+	errV1 := ErrorNew("msg1", errV0)
+	errV2 := ErrorNew2("msg2", errV0, errV1)
+
+	b1, err := json.Marshal(errV1)
+	if err != nil {
+		t.Fatal("error v1 fail", err)
+	}
+	b2, err := json.Marshal(errV2)
+	if err != nil {
+		t.Fatal("error v1 fail", err)
+	}
+
+	if `{"code":50000,"msg":"msg1","ie":"ttt"}` != string(b1) {
+		t.Fatal("Error 1 fail", string(b1))
+	}
+
+	if `{"code":50000,"msg":"msg2","ie":"ttt\nmsg1\nttt"}` != string(b2) {
+		t.Fatal("Error 2 fail")
+	}
+}
